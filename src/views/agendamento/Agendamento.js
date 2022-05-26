3aaa316486eead58f436b2410b6dfa6851e859cf
Rijkaard Melo <rijkaardmelo@gmail.com>
2022-05-25 22:33:54 -0300

    main

--- src/views/agendamento/Agendamento.js
+++ src/views/agendamento/Agendamento.js
@@ -1,14 +1,35 @@
+// import { useState } from 'react';
+
+import AppBar from '@mui/material/AppBar';
+// import ToggleButton from '@mui/material/ToggleButton';
+// import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 
 import './Agendamento.css'
 
-export default function Agendamento (){
-    return(
+
+export default function Agendamento() {
+    // const [alignment, setAlignment] = useState('web');
+
+    // const handleChange = (event, newAlignment) => {
+    //     setAlignment(newAlignment);
+    // };
+    return (
         <div id="Agendamento">
-            <div id='agendamentoNav'>
-                <CalendarTodayOutlinedIcon id='agendamentoCalendar'/>
+            <AppBar id='agendamentoNav' >
+                <CalendarTodayOutlinedIcon id='agendamentoCalendar' />
                 <label id='agendamentoOnline'>Agendamento online</label>
-            </div>
+                {/* <ToggleButtonGroup
+                    color="primary"
+                    value={alignment}
+                    exclusive
+                    onChange={handleChange}
+                >
+                    <ToggleButton value="web">Web</ToggleButton>
+                    <ToggleButton value="android">Android</ToggleButton>
+                    <ToggleButton value="ios">iOS</ToggleButton>
+                </ToggleButtonGroup> */}
+            </AppBar>
         </div>
     );
 }
