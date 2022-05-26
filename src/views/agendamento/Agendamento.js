0dd75e7d02b43cce40c7e3fe2c7e87883ad0983a
Rijkaard Melo <rijkaardmelo@gmail.com>
2022-05-26 15:13:39 -0300

    main

--- src/views/agendamento/Agendamento.js
+++ src/views/agendamento/Agendamento.js
@@ -1,34 +1,43 @@
 // import { useState } from 'react';
 
 import AppBar from '@mui/material/AppBar';
-// import ToggleButton from '@mui/material/ToggleButton';
-// import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
+import EditIcon from '@mui/icons-material/Edit';
+import Avatar from '@mui/material/Avatar';
+import { styled } from '@mui/material/styles';
 
 import './Agendamento.css'
+import { Button } from '@mui/material';
 
+const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 14,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#FFFFFF",
+    color: "#000000",
+    fontFamily: 'Poppins',
+    '&:focus': {
+        boxShadow: 'none',
+        backgroundColor: '#0063cc',
+        borderColor: '#0063cc',
+        color: '#FFFFFF',
+    },
+});
 
 export default function Agendamento() {
-    // const [alignment, setAlignment] = useState('web');
-
-    // const handleChange = (event, newAlignment) => {
-    //     setAlignment(newAlignment);
-    // };
     return (
         <div id="Agendamento">
             <AppBar id='agendamentoNav' >
                 <CalendarTodayOutlinedIcon id='agendamentoCalendar' />
                 <label id='agendamentoOnline'>Agendamento online</label>
-                {/* <ToggleButtonGroup
-                    color="primary"
-                    value={alignment}
-                    exclusive
-                    onChange={handleChange}
-                >
-                    <ToggleButton value="web">Web</ToggleButton>
-                    <ToggleButton value="android">Android</ToggleButton>
-                    <ToggleButton value="ios">iOS</ToggleButton>
-                </ToggleButtonGroup> */}
+                <BootstrapButton id='agendamentoMeus' startIcon={<FolderOpenOutlinedIcon />}> <span>Meus agendamentos</span></BootstrapButton>
+                <BootstrapButton id='agendamentoAgendar' startIcon={<EditIcon />}>Agendar</BootstrapButton>
+                <Avatar id='agendamentoAvatar' >LC</Avatar>
             </AppBar>
         </div>
     );
