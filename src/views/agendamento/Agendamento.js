36cba320c76304e29f6652d086df35652eecd7fe
Rijkaard Melo <rijkaardmelo@gmail.com>
2022-05-26 22:19:39 -0300

    main

--- src/views/agendamento/Agendamento.js
+++ src/views/agendamento/Agendamento.js
@@ -1,42 +1,35 @@
-// import { useState } from 'react';
+import useState from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
 import EditIcon from '@mui/icons-material/Edit';
 import Avatar from '@mui/material/Avatar';
-import { styled } from '@mui/material/styles';
 
 import './Agendamento.css'
 import { Button } from '@mui/material';
 
-const BootstrapButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 14,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: "#FFFFFF",
-    borderColor: "#FFFFFF",
-    color: "#000000",
-    fontFamily: 'Poppins',
-    '&:focus': {
-        boxShadow: 'none',
-        backgroundColor: '#0063cc',
-        borderColor: '#0063cc',
-        color: '#FFFFFF',
-    },
-});
-
 export default function Agendamento() {
+    const [styleAgendamentoMeus, setStyleAgendamentoMeus] = useState("agendamentoMeus");
+
+    const changeAgendamentoMeus = () => {
+        setStyleAgendamentoMeus("agendamentoMeusSelect");
+      };
     return (
         <div id="Agendamento">
             <AppBar id='agendamentoNav' >
-                <CalendarTodayOutlinedIcon id='agendamentoCalendar' />
-                <label id='agendamentoOnline'>Agendamento online</label>
-                <BootstrapButton id='agendamentoMeus' startIcon={<FolderOpenOutlinedIcon />}> <span>Meus agendamentos</span></BootstrapButton>
-                <BootstrapButton id='agendamentoAgendar' startIcon={<EditIcon />}>Agendar</BootstrapButton>
+                <Button id='agendamentoOnline'>
+                    <CalendarTodayOutlinedIcon sx={{ fontSize: 40 }} />
+                    <label>Agendamento online</label>
+                </Button>
+                <Button id={styleAgendamentoMeus} onClick={changeAgendamentoMeus}   >
+                    <FolderOpenOutlinedIcon sx={{ fontSize: 30 }} />
+                    <label>Meus agendamentos</label>
+                </Button>
+                <Button id='agendamentoAgendar'>
+                    <EditIcon sx={{ fontSize: 30 }} />
+                    <label>Agendar</label>
+                </Button>
                 <Avatar id='agendamentoAvatar' >LC</Avatar>
             </AppBar>
         </div>
